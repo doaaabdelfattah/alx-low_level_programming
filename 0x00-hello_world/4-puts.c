5f56ec5749ca8095c2da9e925268edff9a053921
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-10 05:07:07 -0700

    [0x00-hello_world]

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main function - main function for all other functions
+ * Return: 0 if program success
+ */
+int main(void)
+{
+	/* *print function* */
+
+	char string[] = "Programming is like building a multilingual puzzle";	
+	
+	puts(string);
+	return (0);
+}
