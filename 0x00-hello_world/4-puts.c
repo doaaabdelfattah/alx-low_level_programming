e8449bca1d690e15167df14a302ce948eb8927c3
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-10 13:47:34 -0700

    [0x00-hello_world]

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,6 +12,6 @@ int main(void)
 	 *print function
 	 */
 
-	puts(\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
