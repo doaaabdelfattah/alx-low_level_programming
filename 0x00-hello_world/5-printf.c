6fa58db64b6bfbe5e8d45b5d5077fc5d421d8840
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-10 13:49:53 -0700

    [0x00-hello_world]

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
-/*
- * first c program
- **/
-
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
 int main(void)
 {
-	/*
-	 *print function
-	 **/
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+        /*
+         *printf function
+         */
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
