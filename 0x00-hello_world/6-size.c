49edf98131640a5b0e92f1d7d7c0100dea097ad1
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-10 14:05:18 -0700

    [0x00-hello_world]

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,8 +19,8 @@ int main(void)
 
 	printf("Size of a char: %c byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %li byte(s)\n", sizeof(q));
-	printf("Size of a long long int: %lli byte(s)\n", sizeof(w));
+	printf("Size of a long int: %d byte(s)\n", sizeof(q));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(w));
 	printf("Size of a float: %f byte(s)\n", sizeof(f));
 	return (0);
 }
