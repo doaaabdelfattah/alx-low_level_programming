5418df0d0080c4efd09f7aa062e8714359c5d9fb
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-10 05:16:01 -0700

    [0x00-hello_world]

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main function - main function for all other functions
- * Return: 0 if program success
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
  */
 int main(void)
 {
-	/* *print function* */
+	/*
+	 *print function
+	 */
 
-	char string[] = "Programming is like building a multilingual puzzle";	
-	
+	char string[] = "Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return (0);
 }
