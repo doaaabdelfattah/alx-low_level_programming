909c2f8b4e8ce54b6f15e4ffc17926e5885d33eb
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 16:53:13 -0700

    [0x01-variables_if_else_while]

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
+int main(void)
+{
+	char alpha1 = 'a';
+	char alpha2= 'A';
+
+	for (alpha1 = 'a'; alpha1 <= 'z' ; alpha1++)
+	{
+		putchar(alpha1);
+	}
+	for (alpha2 = 'A'; c <= 'Z' ; alpha2++)
+        {
+                putchar(alpha2);
+        }
+	putchar('\n');
+	return (0);
+
+}
