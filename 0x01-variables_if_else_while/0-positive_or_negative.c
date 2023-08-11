0a35b1b9dd99b17fca8703da5a1651e8643631a0
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 16:03:26 -0700

    [0x01-variables_if_else_while]

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes */
-        if (n > 0)
-        {
-            printf("%d is positive\n",n);
-        }
-        else if (n < 0)
-        {
-            printf("%d is negative\n",n);
-        }
-        else
-        {
-            printf("%d is zero\n",n);
-        }
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes */
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n",n);
+	}
+	else
+	{
+		printf("%d is zero\n",n);
+	}
+	return (0);
 }
