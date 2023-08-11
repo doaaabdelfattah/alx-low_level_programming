510c316eb2d6d48e3f737624a7f2a271da7b3794
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 16:26:09 -0700

    [0x01-variables_if_else_while]

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes */
+	int q = n % 10;
+
+	if (q > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, q);
+	}
+	else if (q != 0 && q < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, q);
+	}
+	else(q = 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, q);
+	}
+	return (0);
+}
