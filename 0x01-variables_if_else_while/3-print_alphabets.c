5b6dacbbdfa84ea120db9c6fc8e61146ee1b858c
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 16:55:33 -0700

    [0x01-variables_if_else_while]

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,16 +11,16 @@
 int main(void)
 {
 	char alpha1 = 'a';
-	char alpha2= 'A';
+	char alpha2 = 'A';
 
 	for (alpha1 = 'a'; alpha1 <= 'z' ; alpha1++)
 	{
 		putchar(alpha1);
 	}
 	for (alpha2 = 'A'; alpha2 <= 'Z' ; alpha2++)
-        {
-                putchar(alpha2);
-        }
+	{
+		putchar(alpha2);
+	}
 	putchar('\n');
 	return (0);
 
