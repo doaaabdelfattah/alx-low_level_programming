27c461aa18384c34586200ac56da92562295b9ed
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 17:01:32 -0700

    [0x01-variables_if_else_while]

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			return 1;
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
