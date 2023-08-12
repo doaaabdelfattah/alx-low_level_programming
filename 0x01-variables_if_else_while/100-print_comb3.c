493132e755b9eaa0965f6110ee457f5b9be9ba80
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 18:52:55 -0700

    [0x01-variables_if_else_while]

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,21 +12,16 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0 ; num1 < 9 ; num1++)
+	for (num1 = 0 ; num1 <= 9 ; num1++)
 	{
 		for (num2 = 1  ; num2 <= 9 ; num2++)
 		{
-			if (num1 != num2 || num1 < 9)
-			{
-
 			putchar('0' + num1);
 			putchar('0' + num2);
-			putchar(44);
-			putchar(32);
-			}
-			else
+			if (num1 < 8 || num2 < 9)
 			{
-				continue;
+				putchar(44);
+				putchar(32);
 			}
 		}
 	}
