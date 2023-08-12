1dd10d010728d0f9b2929473000b6fbd775a10da
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 18:27:39 -0700

    [0x01-variables_if_else_while]

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 0 ; hex <= 9 ; hex++)
+	{
+		putchar('0' + hex);
+		for (hex = 1 ; hex <= 9 ; hex++)
+		{
+			putchar('0' + hex);
+		}
+		if (hex < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
