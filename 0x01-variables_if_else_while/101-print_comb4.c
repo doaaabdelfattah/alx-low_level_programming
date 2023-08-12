dbb5ac5b9993acbeb46a9a992f63ebefceca7ce8
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 18:58:21 -0700

    [0x01-variables_if_else_while]

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - starting point
+ *
+ *Description: main function for all other functions
+ *
+ *Return: 0 if program success
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0 ; num1 <= 9 ; num1++)
+	{
+		for (num2 = num1 + 1  ; num2 <= 9 ; num2++)
+		{
+			for(num3 = num2 + 1; num3 <=9 ; num3++)
+			{
+			putchar('0' + num1);
+			putchar('0' + num2);
+			if (num1 < 8 || num2 < 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			}
+		}
+	}	
+
+	putchar('\n');
+	return (0);
+
+}
