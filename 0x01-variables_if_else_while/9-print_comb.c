f67649a7081207585b3c01689d56b28c26f8eb8d
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-11 18:11:47 -0700

    [0x01-variables_if_else_while]

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,11 @@ int main(void)
 	for (hex = 0 ; hex <= 9 ; hex++)
 	{
 		putchar('0' + hex);
-		putchar(44);
-		putchar(31);
+		if (hex < 9 )
+		{
+			putchar(44);
+			putchar(31);
+		}
 	}
 
 	putchar('\n');
