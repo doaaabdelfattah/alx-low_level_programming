7a0fd4fa1b66d17c3e7d463306d1a99e06970887
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-14 02:01:13 +0300

    advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,28 +9,28 @@
  */
 int main(void)
 {
-    int num1, num2, num3, num4;
+int num1, num2, num3, num4;
 
-    for (num1 = 0; num1 <= 9; num1++)
-    {
-        for (num2 = 0; num2 < 9; num2++)
-        {
-            for (num3 = 0; num3 <= 9; num3++)
-            {
-                for (num4 = 0; num4 <= 9; num4++)
-                {
-                    putchar('0' + num1);
-                    putchar('0' + num2);
-                    putchar(32);
-                    putchar('0' + num3);
-                    putchar('0' + num4);
-                    putchar(44);
-                    putchar(32);
-                    }
-            }
-        }
-    }
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = 0; num2 < 9; num2++)
+{
+for (num3 = 0; num3 <= 9; num3++)
+{
+for (num4 = 0; num4 <= 9; num4++)
+{
+putchar('0' + num1);
+putchar('0' + num2);
+putchar(32);
+putchar('0' + num3);
+putchar('0' + num4);
+putchar(44);
+putchar(32);
+}
+}
+}
+}
 
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
