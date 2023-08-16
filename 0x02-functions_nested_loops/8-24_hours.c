d71cac237cb1cbcedcad1743747eeccf388eadfa
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 16:07:45 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *
+ *jack_bauer - print every minute of the day
  *
  */
 void jack_bauer(void)
@@ -8,7 +8,7 @@ void jack_bauer(void)
 int num1, num2, num3, num4;
 for (num1 = 0; num1 <= 2; num1++)
 {
-for (num2 = 0; num2 <= 9; num2++)
+for (num2 = 0; num2 <= 3; num2++)
 {
 for (num3 = 0; num3 <= 5; num3++)
 {
