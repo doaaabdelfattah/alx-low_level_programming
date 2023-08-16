874e77ab95687b6e33865f41c0e610af2bcbf774
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 14:36:50 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,17 @@
 #include "main.h"
-/*
+
+/**
+ *_isalpha - function to check for alphabetical character
+ *@c: The number to be check
  *
+ * Return: 1 if the character lowercase. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
