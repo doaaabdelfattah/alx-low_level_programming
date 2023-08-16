53a1bbb24fafc3ad8b8d1d9ddd8a62dbc2ad2f88
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 16:26:05 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,14 @@ void jack_bauer(void)
 {
 int h, m;
 for (h = 0; h <= 23; h++)
-{	for (m = 0; m <=59; m++)
+{
+	for (m = 0; m <= 59; m++)
 	{
 		_putchar((h / 10) + '0');
 		_putchar((h % 10) + '0');
 		_putchar(58);
 		_putchar((m / 10) + '0');
-                _putchar((m % 10) + '0');
+		_putchar((m % 10) + '0');
 		_putchar('\n');
 	}
 
