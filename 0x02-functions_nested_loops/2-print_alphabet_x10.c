59b70a73d5258ab8c77c93e66e4168c6a9549602
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 14:10:04 -0700

    [0x02-functions_nested_loops]

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - function print for all letters
+ */
+void print_alphabet_x10(void);
+	{
+	int count;
+	for (count = 0; i < 10; i++)
+	{
+		char c = 'a';
+		for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	}
