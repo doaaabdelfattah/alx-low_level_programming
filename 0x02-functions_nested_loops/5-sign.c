f36508e5a03968a0f01fb32675ee20b2f06a9f4d
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 14:55:53 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,30 @@
 #include "main.h"
 /**
+ *print_sign - check signs
+ *@n: The number to be checked
  *
+ * Return: 1 if it's true. 0 otherwise
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		char c = '+';
+		
 		_putchar(c);
 		return (1);
 	}
 	if (n == 0)
 	{
 		char c = '0';
+		
 		_putchar(c);
 		return (0);
 	}
 	else
 	{
 		char c = '-';
+		
 		_putchar(c);
 		return (-1);
 	}
