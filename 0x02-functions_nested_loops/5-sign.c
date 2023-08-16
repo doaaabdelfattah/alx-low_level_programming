9443d2fce1da97f3788007c2f81f9bf44e2b365e
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 14:52:38 -0700

    [0x02-functions_nested_loops]

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		char c = '+';
+		_putchar(c);
+		return (1);
+	}
+	if (n = 0)
+	{
+		char c = '0';
+		_putchar(c);
+		return (0);
+	}
+	else
+	{
+		char c = '-';
+		_purchar(c);
+		return (-1);
+	}
+}
