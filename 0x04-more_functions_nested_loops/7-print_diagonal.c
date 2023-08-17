77271107e7145dd3533013ca7a91d5afe4ddfbd1
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 15:32:30 -0700

    [0x04-more_functions_nested_loops]

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal -  draw diagonal line
+ * @n: number of times the character _
+ */
+
+void print_diagonal(int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+		_putchar('');
+	_putchar('\');
+	_putchar('\n');
+}
