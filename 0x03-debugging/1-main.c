07e7968d462fdbce956a6457412178c015173257
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 12:36:14 -0700

    [0x03-debugging]

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -10,14 +9,15 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 while (i < 10)
 {
 putchar(i);
 i++;
 }
 
-/* i never increments so it will always less than 10*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
