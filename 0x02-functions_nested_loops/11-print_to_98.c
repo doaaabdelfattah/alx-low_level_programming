1eab901b6f68be40e92eff0aeb09cfcab23c6625
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 18:11:04 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 /**
  *
- *
+ *print_to_98 -  prints all natural numbers from n to 98, followed by a new line
  *
  */
 void print_to_98(int n)
 {
-if ( n < 98)
+if (n < 98)
 {
-for(; n <98;n++)
+for(; n < 98; n++)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 }
-printf("%d\n",n);
+printf("%d\n", n);
 }
 
 else
 {
-for(; n> 98; n--)
+for(; n > 98; n--)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 }
-printf("%d\n",n);
+printf("%d\n", n);
 }
 }
