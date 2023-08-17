acbdcc851e607fbb48f69c206ab308cd1076c283
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 18:09:08 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,21 @@
  */
 void print_to_98(int n)
 {
-if ( n <= 98)
+if ( n < 98)
 {
-for(; n <=98;n++)
+for(; n <98;n++)
 {
 printf("%d, ",n);
 }
+printf("%d\n",n);
 }
 
 else
 {
-for(; n>= 98; n--)
+for(; n> 98; n--)
 {
 printf("%d, ",n);
 }
+printf("%d\n",n);
 }
 }
