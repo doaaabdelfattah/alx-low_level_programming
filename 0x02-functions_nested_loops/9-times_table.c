cf3847f65d013e969f099a1722e4dc993f43977c
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 17:19:54 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,10 +11,14 @@ void times_table(void)
 	for (i = 0; i <= 9; i++)
 	{
 	int k = n * i;
-	if ( k <= 9)
+	
+	/* If the number is one digit*/
+	if (k >= 9)
 	{
-		if ( n == 0)
-		_putchar(k + '0');
+		/* No spaces on the first column*/
+		if (i == 0)
+		_putchar(k + '0')
+		/* on other columns */
 		else
 		{	
 		_putchar(32);
@@ -22,15 +26,18 @@ void times_table(void)
 		_putchar(k + '0');
 		}
 	}
+	/* if the number 2 digits */
 	else 
 	{
 	_putchar(32);
 	_putchar((k/10) + '0');
 	_putchar((k % 10) + '0');
 	}
+	/* comma after each number except last number */
 	if ( i < 9)
 	_putchar(',');
 	}
+	/*new line*/
 _putchar('\n');
 }
 }
