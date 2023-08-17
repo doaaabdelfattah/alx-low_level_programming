bf10455b9ccf62f586c65c716726a19aaafcf2b0
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-16 17:55:13 -0700

    [0x02-functions_nested_loops]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
@@ -6,32 +7,14 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
+if (n <= 9)
 {
-for (; n <= 98; n++)
-{
-if (n <=9)
-{
-_putchar(n + '0');
-_putchar(',');
-_putchar(' ');
+	for(; n <=98;n++)
+	printf("%d \,",n);
 }
 else
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-if (n > 98)
-{for (; n >= 98; n--)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
+	for(; n>= 98; n--)
+		printf("%d \,",n);
 }
 }
