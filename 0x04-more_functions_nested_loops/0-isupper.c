87353c30977dc4c3a3800fedab0d58d66ae61be8
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 13:41:29 -0700

    [0x04-more_functions_nested_loops]

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - function to check for uppercase character
+ *@c: The number to be check
+ *
+ * Return: 1 if the character lowercase. 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+/* Function Declaration */
+
+int _isupper(int c);
+
+
+#endif
