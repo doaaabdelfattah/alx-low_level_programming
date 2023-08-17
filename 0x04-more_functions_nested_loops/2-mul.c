1f76473686403a443035ac3b5c66be27513e7219
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 13:51:42 -0700

    [0x04-more_functions_nested_loops]

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - Function mutiply of two integer
+ *@a: the first number
+ *@b: the second number
+ *
+ * Return: The multiply of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
