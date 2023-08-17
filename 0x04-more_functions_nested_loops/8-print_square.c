a5062c7b1e21c223c3e6b4959c56e5c8cc9b85da
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 15:55:09 -0700

    [0x04-more_functions_nested_loops]

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_square -  prints a square
+ *&size: the size of the square
+ *
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		_putchar(35);
+	_putchar('\n');
+	}
+
+
+}
