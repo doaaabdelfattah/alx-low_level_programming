1922ddde639aac782f141c6a2001b9492d8fa48f
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 16:07:57 -0700

    [0x04-more_functions_nested_loops]

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,15 +9,13 @@
 void print_square(int size)
 {
 	int row, col;
-	if (size > 0)
+
+	for (row = 0; row <= size; row++)
 	{
-	for (row =0 ; row < size; row++)
-	{
-		for (col = 0; col < size; col++)
+		for (col = 0; col < row; col++)
 			_putchar(35);
 		_putchar('\n');
 	}
-	}
-	else
-	_putchar('\n');
+
+
 }
