a5cf1c978f4ac1c7466f61e47a9d9b6ce90adca7
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-17 17:20:53 -0700

    [0x04-more_functions_nested_loops]

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,13 @@ int main(void)
 	for (i = 1 ; i <= 100; i++)
 	{
 		if ((i % 5 == 0) && (i % 3 == 0))
-		printf("FizzBuzz");
+		printf("FizzBuzz ");
 		else if (i % 5 == 0)
-		printf("Buzz");
+		printf("Buzz ");
 		else if (i % 3 == 0)
-		printf("Fizz");
+		printf("Fizz ");
 		else
-		printf("%d", i);
-
-		printf(" ");
+		printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
