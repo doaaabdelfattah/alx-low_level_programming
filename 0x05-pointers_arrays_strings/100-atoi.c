0e2b7c725bfb14459b120ae1b534713365625373
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 01:54:55 +0300

    [0x05-pointers_arrays_strings]

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int _atoi(char *s)
+{
+    int count, i, len = _strlen(s);
+    count = 0;
+    for (i = 0; i < len; i++)
+    {
+       count = int s[i] + count;
+        return count;
+    }
+}
+
