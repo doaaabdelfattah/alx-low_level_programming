6dd98ddf428e121a729dbca007fa19e6bea64103
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 00:44:46 +0300

    [0x05-pointers_arrays_strings]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -29,7 +29,9 @@ void print_array(int *a, int n)
     int i;
     for (i = 0; i <= n-1 ; i++)
     {
-        printf("%i, ", a[i]);
+        printf("%i", a[i]);
+        if (i !== n)
+        printf(", ");
     }
     _putchar('\n');
 }
