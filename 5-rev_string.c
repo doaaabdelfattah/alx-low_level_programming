4953b2de393958c6e990360254e31082c89b3171
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-21 23:14:17 +0300

    [0x05-pointers_arrays_strings]

---
+++ 5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int len = _strlen(s);
+
+for (i = 1; i <= len / 2; i++)
+	{
+s[i] = s[len - (i + 1)];
+	}
+}
