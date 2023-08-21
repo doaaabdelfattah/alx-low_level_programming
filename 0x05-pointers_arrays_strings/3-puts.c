0ff7e88c12f660db0d687efdfbb07b2c23ff8ad4
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-21 02:17:55 -0700

    [0x05-pointers_arrays_strings]

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: string to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	for (int = 0; i < _strlen(char *str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
