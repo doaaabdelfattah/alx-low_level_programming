daa8d92a881f8f93c17fd1e5e456eba34c1d4a25
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 00:01:51 +0300

    [0x05-pointers_arrays_strings]

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * puts_half - function that prints half of a string
+ * @str: string to be printed
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, len = _strlen(str);
+
+	while (i < len /2)
+	{
+		_putchar(str[len - i - 1]);
+		i++;
+	}
+	_putchar('\n');
+}
+
