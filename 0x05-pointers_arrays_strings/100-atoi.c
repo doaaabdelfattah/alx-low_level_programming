faa20d155b9c979156839f388f39be6ebc4d91e8
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 02:45:31 +0300

    [0x05-pointers_arrays_strings]

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,33 @@
 
 int _strlen(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+while (s[i] != '\0')
+{
+i++;
 }
-
+return (i);
+}
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s : string
+ *
+ * Return: the number
+ */
 int _atoi(char *s)
 {
-    int count, i, len = _strlen(s);
-    count = 0;
-    for (i = 0; i < len; i++)
-    {
-        if (s[i]>= '0' && s[i]<= '9')
-        {
-        count = (s[i] - 48) + count *10;
-        if(s[i-1] == '-')
-        count = - count;
-        }
-    }
-    return count;
+int count, i, len = _strlen(s);
+count = 0;
+for (i = 0; i < len; i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+count = (s[i] - 48) + count * 10;
+if (s[i - 1] == '-')
+count = -count;
+}
+}
+return (count);
 }
 
