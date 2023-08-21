9c52ae44057416bbe7c0b8c17e8ea9061592eda5
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 02:56:08 +0300

    [0x05-pointers_arrays_strings]

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,14 +26,17 @@ int _atoi(char *s)
 {
 int count, i,sign, len = _strlen(s);
 count = 0;
+sign = 1;
+
 for (i = 0; i < len; i++)
 {
 if (s[i] >= '0' && s[i] <= '9')
 {
 count = (s[i] - 48) + count * 10;
-}
 if (s[i - 1] == '-')
 sign = -1;
+}
+
 }
 return (count * sign);
 }
