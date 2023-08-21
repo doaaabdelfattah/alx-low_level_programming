26135255e9e6f7498050f35d82c5b8403836d2c4
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-21 12:43:14 -0700

    [0x05-pointers_arrays_strings]

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,23 @@
 #include "main.h"
-#include <string.h>
 /**
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: string to be reversed
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 void print_rev(char *s)
 {
 	int i;
-	int len = strlen(s);
+	int len = _strlen(s);
 	
 	for (i = 0; i <= len; i++)
 	{
