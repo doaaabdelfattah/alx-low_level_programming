1a7ace6f18235ed165272704149c6e9592bbdfa6
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-08-21 01:58:43 -0700

    [0x05-pointers_arrays_strings]

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+	return (i);
+}
+
