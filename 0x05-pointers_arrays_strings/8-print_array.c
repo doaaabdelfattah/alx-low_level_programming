5c6cbd6138fd3afcdf90cebe09cdf19543f73531
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-22 00:48:34 +0300

    [0x05-pointers_arrays_strings]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,29 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * _strlen - function that returns the length of a string
- * @s : string
- *
- * Return: lenght of string
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 /**
  * print_array - prints n elements of an array of integers
  * @a: pointer to array
  * @n: the number of element
  *
  */
+
 void print_array(int *a, int n)
 {
     int i;
@@ -35,4 +18,5 @@ void print_array(int *a, int n)
             printf(", ");
         }
     }
+    printf("\n");
 }
