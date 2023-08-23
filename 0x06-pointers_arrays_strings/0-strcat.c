8a3e14a93368665ad33ce71e69b32ec9e4ce075e
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-24 00:23:14 +0300

    [0x06-pointers_arrays_strings]

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -26,16 +26,16 @@ int _strlen(char *s)
 
 char *_strcat(char *dest, char *src)
 {
-    int destlen =  _strlen(dest);
-    int srclen =  _strlen(src);
-    int i;
+int destlen =  _strlen(dest);
+int srclen =  _strlen(src);
+int i;
 
 
-    for (i = 0; i <= srclen; i++)
-    {
-        dest[destlen + i]= src[i];
-    }
-    dest[destlen + i]='\0';
-    return dest;
+for (i = 0; i <= srclen; i++)
+{
+dest[destlen + i] = src[i];
+}
+dest[destlen + i] ='\0';
+return (dest);
 
 }
