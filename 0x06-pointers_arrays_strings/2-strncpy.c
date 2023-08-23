d25642aa924f9dcb1164603658d666203ba6d2ec
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-24 01:29:45 +0300

    [0x06-pointers_arrays_strings]

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  *_strncpy - function that copy string.
  *@dest: the first pointer
  *@src: string to be copied
@@ -11,11 +29,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-
+int srclen = _strlen(src);
 for (i = 0; i < n  && dest[i] != '\0' ; i++)
 {
 	dest[i] = src[i];
 }
-return (dest);
+if (n > srclen)
+dest[i] = '\0';
 
+return (dest);
 }
