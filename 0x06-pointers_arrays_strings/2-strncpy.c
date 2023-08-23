142106a4a79509a31ababebeeba1c95ab0840c4c
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-24 01:35:33 +0300

    [0x06-pointers_arrays_strings]

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -29,13 +29,11 @@ int _strlen(char *s)
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-int srclen = _strlen(src);
 
 for (i = 0; i < n  && src[i] != '\0' ; i++)
 {
 	dest[i] = src[i];
 }
 dest[i] = '\0';
-
 return (dest);
 }
