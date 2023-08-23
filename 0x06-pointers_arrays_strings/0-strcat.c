cf33a4b5d5b7d4acee2336ca6b9dde0cb6b9a534
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-24 00:13:40 +0300

    [0x06-pointers_arrays_strings]

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcat - function that concatenates two strings.
+ *@dest: the first pointer
+ *@src: string to be copied
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int destlen =  _strlen(dest);
+    int srclen =  _strlen(src);
+    int i, j;
+
+
+    for (i = 0; i <= srclen; i++)
+    {
+        dest[destlen + i]= src[i];
+        i++;
+    }
+    dest[destlen + i]='\0';
+    return dest;
+
+}
