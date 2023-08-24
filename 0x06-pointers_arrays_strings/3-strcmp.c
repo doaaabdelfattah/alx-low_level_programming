22d480dbef6e355be00039081bfab9256656c0ea
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-24 22:58:26 +0300

    [0x06-pointers_arrays_strings]

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,18 @@
  * Return: integer
  */
 
-
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
+    int i, result;
 
-for (i = 0; s1[i] != '\0' || s2[i] != '\0' ; i++)
-{
-if (s1[i] != s2[i])
-{
-j =(int) s1[i] - (int) s2[i];
-}
-}
-return (j);
+    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            result = s1[i] - s2[i];
+        }
+        else
+        continue;
+    }
+    return (result);
 }
