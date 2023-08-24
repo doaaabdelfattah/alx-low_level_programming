a76faa1b707ef104ff4bb7fb30da6ac3bd350766
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-25 00:04:05 +0300

    [0x06-pointers_arrays_strings]

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,6 +8,7 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (i == 0)
@@ -19,21 +20,16 @@ char *cap_string(char *s)
 				continue;
 			}
 		}
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-			s[i] == ',' || s[i] == '.' || s[i] == ';' || s[i] == ' ' || s[i] == '?' || s[i] == '!' || s[i] == '\"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+			if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+			s[i] == ',' || s[i] == '.' || s[i] == ';' || s[i] == '?' || s[i] == '!' ||
+			s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 		{
 			i++;
 			if (s[i] >= 'a' && s[i] <= 'z')
 				{
 					s[i] = s[i] - 32;
-					i++;
 					continue;
 				}
-		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-				s[i] = s[i] + 32;
 		}
 	}
 	return (s);
