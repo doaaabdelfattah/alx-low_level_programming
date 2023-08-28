507f2c0a60b180a4c5a4f2e9f4840ce283b1bef4
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-28 17:04:29 +0300

    [0x07-pointers_arrays_strings]

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 int i;
 for (i = 0; s[i] != '\0'; i++)
 {
-    if (s[i] == c)
-    {
-        return (s+i);
-        break;
-    }
+if (s[i] == c)
+{
+return (s + i);
+break;
+}
 }
 return (NULL);
 }
