3e685df9a4900945674ae008de2f34bc7ba5780a
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-28 16:41:39 +0300

    [0x07-pointers_arrays_strings]

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  *_memset - function that fills memory with a constant byte.
- *@s:
- *@b:
- *@n:
- *Return: 
- * 
+ *@s: the memory to be filled
+ *@b: charatcer
+ *@n: number of bytes
+ *Return: pointer to character
+ *
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
-    for (i = 0; i <= n; i++)
-    {
-        s[i] = b;
-    }
-    return (s);
+unsigned int i;
+for (i = 0; i <= n; i++)
+{
+s[i] = b;
+}
+return (s);
 
 }
