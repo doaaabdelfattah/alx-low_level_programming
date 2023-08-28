dbd87458a567cc31504c197ee7036923754dd5f6
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-28 17:02:17 +0300

    [0x07-pointers_arrays_strings]

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - function locates a character in a string.
+ *@s: string
+ *@c: character to be located
+ *Return: pointer to character
+ *
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+    if (s[i] == c)
+    {
+        return (s);
+        break;
+    }
+}
+return (NULL)
+}
