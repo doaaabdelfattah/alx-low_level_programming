705848c741e614457afbcbbaa0a7a010385ce52d
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-28 23:04:29 +0300

    [0x07-pointers_arrays_strings]

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
+int i = 0;
+int j = 0;
 unsigned int len = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[i] != '\0')
 {
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-{
-len++;
-}
-break;
-}
+    while (accept[j] != '\0' && s[i] != accept[j])
+    {
+        len++;
+        j++;
+    }
+    i++;
 }
 return (len);
 }
