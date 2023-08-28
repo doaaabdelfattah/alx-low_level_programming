9348ac7d6cf2c8b07cd8aebc6fe00b2cf6ca64c6
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-08-29 01:25:53 +0300

    [0x07-pointers_arrays_strings]

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,21 +9,13 @@
 
 void print_diagsums(int *a, int size)
 {
-int i, j;
+int i;
 int d1 = 0;
 int d2 = 0;
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
-{
-if (i == j)
 {
 d1 = d1 + a[i];
-}
-if (i == (size - j - 1))
-{
-d2 = d2 + a[i];
-}
 }
 }
 printf("%i, %i", d1, d2);
