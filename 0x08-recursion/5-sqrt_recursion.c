754a2e2b26988c3927254c9012e307a02e8c7f08
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 02:43:10 +0300

    [0x08-recursion]

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,36 @@
 #include "main.h"
 int _root(int n, int r);
 /**
- * _sqrt_recursion -  returns the natural square root of a number.
+ * _root - returns the root.
+ *@n: the number
+ *@r: root
+ *Return: the natural square
+ */
+int _root(int n, int r)
+{
+if (r * r == n)
+{
+return (r);
+}
+else
+return (_root(n, r + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
  *@n: the number
  *Return: the natural square
  */
 
-int _root(int n, int r)
-{
-    if ( r * r == n )
-    {
-        return (r);
-    }
-else
-    return ( _root(n, r + 1));
-}
 
 int _sqrt_recursion(int n)
 {
-    int r = 0 ;
-    if (n == 1)
-    return (1);
+int r = 0;
+if (n == 1)
+return (1);
 
-    if (n < 0)
-    return (-1);
+if (n < 0)
+return (-1);
 
-    return (_root(n, r));
+return (_root(n, r));
 
 }
