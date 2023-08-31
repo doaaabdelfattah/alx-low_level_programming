c3c295bae3361ffb327c5ceeaaec61774278d4da
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 00:27:31 +0300

    [0x08-recursion]

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed
  */
 
 void _puts_recursion(char *s)
 {
-    if(*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
