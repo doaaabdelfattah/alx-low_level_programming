73e1f6e08673b7a6ae3b4e4c41b33250ee2392a0
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-02 00:49:15 +0300

    [0x0A-argc_argv]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+int _isdigit(int c);
+
+
+/**
+ *_isdigit - function to check for digit character
+ *@c: The number to be check
+ *
+ * Return: 1 if the character lowercase. 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+return (0);
+}
+
 /**
  * main - main function
  * @argc: count
@@ -24,7 +42,7 @@ for (i = 1; i <= argc - 1; i++)
 {
 for (j = 0, n = strlen(argv[i]); j < n; j++)
 {
-if (!isdigit(argv[i][j]))
+if (!_isdigit(argv[i][j]))
 {
 printf("Error \n");
 return (1);
