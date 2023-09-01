149747216080f4329e7dd55cf11163d53dd867a6
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 04:27:52 +0300

    [0x08-recursion]

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,12 @@
 #include "main.h"
- int _check_palind(char *s, int i);
- int _strlen(char *s);
+int _check_palind(char *s, int i);
+int _strlen(char *s);
 
 /**
- * _strlen_recursion - returns the length of a string.
+ * _strlen - returns the length of a string.
  *@s: the string
- *Return: length
- */
+*Return: length
+*/
 int _strlen(char *s)
 {
 if (*s == '\0')
@@ -21,29 +21,26 @@ return (1 + _strlen(s + 1));
  * @s: pinter to string
  * @i: count
  *Return: 1 if string is palindrome otherwise 0.
- */
- int _check_palind(char *s, int i)
+*/
+int _check_palind(char *s, int i)
 {
-    int n = _strlen(s);
-    if (i < n /2)
-    {
-        if (s[i] != s[n - 1 - i])
-        return (_check_palind(s, i++));
-    }
-    return (1);
+int n = _strlen(s) - 1;
+if (n == 0)
+return (1);
+
+if (s[i] == s[n - i])
+return (_check_palind(s, i++));
+return (0);
 }
 
 /**
  * is_palindrome - check if string is palindrome.
  * @s: pinter to string
  *Return: 1 if string is palindrome otherwise 0.
- */
+*/
 
 int is_palindrome(char *s)
 {
-    int i = 0;
-    if (_strlen(s) == 0)
-    return (1);
-    else
-    return _check_palind(s, i);
+int i = 0;
+return (_check_palind(s, i));
 }
