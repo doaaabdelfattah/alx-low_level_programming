9a05ec24365e70c9588a1866729c4857af7c961e
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-02 01:09:51 +0300

    [0x0A-argc_argv]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-int _isdigit(int c);
 
-/**
- *_isdigit - function to check for digit character
- *@c: The number to be check
- *
- * Return: 1 if the character lowercase. 0 otherwise
- */
-int _isdigit(int c)
-{
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-else
-return (0);
-}
+// int _isdigit(int c);
+
+// /**
+//  *_isdigit - function to check for digit character
+// *@c: The number to be check
+// *
+// * Return: 1 if the character lowercase. 0 otherwise
+// */
+// int _isdigit(int c)
+// {
+// if (c >= 48 && c <= 57)
+// {
+// return (1);
+// }
+// else
+// return (0);
+// }
 
 /**
  * main - main function
@@ -29,7 +29,7 @@ return (0);
 
 int main(int argc, char *argv[])
 {
-int i, j, n;
+int i;
 int count = 0;
 
 if (argc == 1)
@@ -39,16 +39,13 @@ else
 {
 for (i = 1; i <= argc - 1; i++)
 {
-for (j = 0, n = strlen(argv[i]); j < n; j++)
-{
-if (!_isdigit(argv[i][j]))
+
+if (!atoi(argv[i]))
 {
 printf("Error \n");
 return (1);
 }
 else
-continue;
-}
 count = count + atoi(argv[i]);
 }
 printf("%i\n", count);
