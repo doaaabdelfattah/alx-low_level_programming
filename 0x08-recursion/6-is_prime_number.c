4aec4e1f95948f03ac99f94e71ccc4757e3accbe
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 03:10:23 +0300

    [0x08-recursion]

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,18 @@
 #include "main.h"
 int _check_prime(int n, int i);
 
-
 int _check_prime(int n, int i)
 {
-    if (n % i != 0)
-    return (_check_prime(n, i + 1));
-else
-    return (1);
+    if (i <= n / 2)
+    {
+        if (n % i != 0)
+        {
+            return (_check_prime(n, i + 1));
+        }
+        else
+        return (1);
+    }
+    return (0);
 }
 /**
  * is_prime_number - check if the number is prime
@@ -19,7 +24,7 @@ int is_prime_number(int n)
 {
     int i = 2;
     if (n == 0 || n == 1)
-    return(0);
+        return (0);
     else
-    return (_check_prime(n, i));
+        return (_check_prime(n, i));
 }
