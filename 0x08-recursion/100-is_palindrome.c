da0656a9af95fa02bd6f3735ad7e35d6481cc5fc
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 05:01:22 +0300

    [0x08-recursion]

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,16 +25,20 @@ return (1 + _strlen(s + 1));
 */
 int _check_palind(char *s, int len, int i)
 {
-if (i <= len / 2)
+
+if (i < len / 2)
 {
+
 if (s[i] == s[len - i - 1])
-return (_check_palind(s, len, i++));
-else
+{
+_check_palind(s, len, i++);
 return(1);
 }
 
-
-return (0);
+else
+return(0);
+}
+return (1);
 }
 
 /**
