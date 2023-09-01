c1588ccfbf0e1da972aec9ee0e31559ef595c688
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-02 02:02:05 +0300

    [0x0A-argc_argv]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,12 +20,15 @@ else
 {
 for (i = 1; i <= argc - 1 ; i++)
 {
-if (!atoi(argv[i]) && atoi(argv[i]) > 0)
+if (!atoi(argv[i]))
 {
 printf("Error \n");
 return (1);
 }
+if (atoi(argv[i]) > 0)
+{
 count = count + atoi(argv[i]);
+}
 }
 printf("%d\n", count);
 }
