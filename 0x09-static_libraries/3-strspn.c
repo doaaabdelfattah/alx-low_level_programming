6c8c386bd86ac632a34b202832a4534ed264de26
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-01 13:22:01 +0300

    [0x09-static_libraries]

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strspn - function that gets the length of a prefix substring.
+ *@s: string
+ *@accept: bytes to be scanned
+ *Return: pointer to character
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+unsigned int len = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+len++;
+break;
+}
+else if (accept[j + 1] == '\0')
+return (len);
+}
+}
+return (len);
+}
+
