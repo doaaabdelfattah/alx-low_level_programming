147a84dd3b46e5b1d1bc68f80cdff0dbff57a515
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-02 00:46:52 +0300

    [0x0A-argc_argv]

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - main function
+ * @argc: count
+ * @argv: array
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j, n;
+int count = 0;
+
+if (argc == 1)
+printf("0\n");
+
+else
+{
+for (i = 1; i <= argc - 1; i++)
+{
+for (j = 0, n = strlen(argv[i]); j < n; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error \n");
+return (1);
+}
+else
+continue;
+}
+count = count + atoi(argv[i]);
+}
+printf("%i\n", count);
+}
+}
