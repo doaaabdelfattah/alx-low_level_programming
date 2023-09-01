9f1f3abd4a5f91327ebfc4e198b8b2ddef8631c1
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-02 01:50:07 +0300

    [0x0A-argc_argv]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,28 +10,25 @@
 
 int main(int argc, char *argv[])
 {
-    int i, j;
-    int count = 0;
+int i;
+int count = 0;
 
-    if (argc == 1)
-        printf("0\n");
+if (argc == 1)
+printf("0\n");
 
-    else
-    {
-        for (i = 1; i <= argc - 1; i++)
-        {
-            for (j = 0; argv[i][j] != '\0'; j++)
-            {
-                if (argv[i][j] > '9' || argv[i][j] < '0')
-                {
-                    printf("Error \n");
-                    return (1);
-                }
-            }
-        }
-        count = count + atoi(argv[i]);
-        printf("%d\n", count);
-    }
+else
+{
+for (i = 1; i <= argc - 1; i++)
+{
+if (!atoi(argv[i]))
+{
+printf("Error \n");
+return (1);
+}
+count = count + atoi(argv[i]);
+}
+printf("%d\n", count);
+}
 
-    return (0);
+return (0);
 }
