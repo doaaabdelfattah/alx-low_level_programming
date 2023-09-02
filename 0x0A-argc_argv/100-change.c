899ff2ffd4df098b241f6863c73bc876f6f04f8e
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-09-01 18:47:09 -0700

    [0x0A-argc_argv]

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main function
- *
- *
+ * @argc: count
+ * @argv: array
+ * Return: 0 if success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+
 }
