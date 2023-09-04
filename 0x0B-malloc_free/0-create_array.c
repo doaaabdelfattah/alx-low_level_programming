877e48b0ba1d399d90e95434ab82946827e08493
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-04 11:28:37 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,9 @@ char *create_array(unsigned int size, char c)
 {
 char *ptr = (char*)malloc(size);
 unsigned int i;
-for (i = 0; i < size; i++)
+for (i = 0; i <= size; i++)
 {
 *(ptr+i) = c;
 }
-
 return (ptr);
-
 }
