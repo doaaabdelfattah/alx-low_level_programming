9d2329aff01ea7666a665a97cb8a5fdebb183fc7
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-04 22:41:51 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,8 +27,8 @@ return (i);
 
 char *str_concat(char *s1, char *s2)
 {
-int len1 = _strlen(s1);
-int len2 = _strlen(s2);
+int len1 = strlen(s1);
+int len2 = strlen(s2);
 int total_size = (len1 + len2 + 1);
 
 char *ptr;
