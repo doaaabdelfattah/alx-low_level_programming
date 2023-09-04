0eda32a6c5154bcbf7f999e280256e59b1fecb16
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-04 12:53:42 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,13 @@
 
 int _strlen(char *s)
 {
-    int i = 0;
+int i = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
 }
 /**
  * str_concat -  concatenates two strings.
@@ -27,35 +27,35 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
-    int len1 = _strlen(s1);
-    int len2 = _strlen(s2);
-    int total_size = (len1 + len2 + 1);
+int len1 = _strlen(s1);
+int len2 = _strlen(s2);
+int total_size = (len1 + len2 + 1);
 
-    char *ptr;
-    int i;
-    if (s1 == NULL)
-    {
-        *s1 = '\0';
-        len1 = 0;
-    }
-    if (s2 == NULL)
-    {
-        *s2 = '\0';
-        len2 = 0;
-    }
+char *ptr;
+int i;
+if (s1 == NULL)
+{
+*s1 = '\0';
+len1 = 0;
+}
+if (s2 == NULL)
+{
+*s2 = '\0';
+len2 = 0;
+}
 
-    ptr = (char *)malloc(total_size);
-    if (ptr == NULL)
-        return (NULL);
+ptr = (char *)malloc(total_size);
+if (ptr == NULL)
+return (NULL);
 
-    for (i = 0; i < len1; i++)
-    {
-        *(ptr + i) = *(s1 + i);
-    }
-    for (i = 0; i < len2; i++)
-    {
-        *(ptr + len1 + i) = *(s2 + i);
-    }
-    *(ptr + total_size) = '\0';
-    return (ptr);
+for (i = 0; i < len1; i++)
+{
+*(ptr + i) = *(s1 + i);
+}
+for (i = 0; i < len2; i++)
+{
+*(ptr + len1 + i) = *(s2 + i);
+}
+*(ptr + total_size) = '\0';
+return (ptr);
 }
