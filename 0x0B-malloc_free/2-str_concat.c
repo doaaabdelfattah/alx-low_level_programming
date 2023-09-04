26d8e7179e2dea1f1cc1628333f5f8bd8f7c5825
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-04 12:52:40 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,12 +33,12 @@ char *str_concat(char *s1, char *s2)
 
     char *ptr;
     int i;
-    if (*s1 == 0)
+    if (s1 == NULL)
     {
         *s1 = '\0';
         len1 = 0;
     }
-    if (*s2 == 0)
+    if (s2 == NULL)
     {
         *s2 = '\0';
         len2 = 0;
