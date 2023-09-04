33fb4ecb7818216db86f0ba96e3878b8e00d955c
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-05 00:35:10 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -38,7 +38,7 @@ char *_strdup(char *str)
 
 	for (size = 0; str[size] != '\0'; size++)
 		continue;
-	ptr = (char *)malloc(size);
+	ptr = (char *)malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
