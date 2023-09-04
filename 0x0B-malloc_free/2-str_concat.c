c9a41ecfb5d280f6218af5cd4f149a55387d6261
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-04 22:06:42 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -39,13 +39,11 @@ if (ptr == NULL)
 return (NULL);
 
 for (i = 0; i < len1; i++)
-{
 *(ptr + i) = *(s1 + i);
-}
+
 for (i = 0; i < len2; i++)
-{
 *(ptr + len1 + i) = *(s2 + i);
-}
+
 *(ptr + total_size) = '\0';
 return (ptr);
 }
