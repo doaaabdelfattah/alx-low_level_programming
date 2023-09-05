0e8a65d174fe4642fa35acd7b72680f22f8d520a
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-06 02:27:19 +0300

    [0x0B-malloc_free]

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @c: integer
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: integer
  * @av: pointer
  * Return: pointer to string
  */
@@ -19,27 +19,13 @@ array = malloc(ac + 1);
 if (array == NULL)
 return (NULL);
 
-// for (i = 0 ; i < ac; i++)
-// {
-// array[i] = (char *)malloc(sizeof(av[i]));
-// if (array[i] == NULL)
-// {
-// for (j = 0; j <= i; j++)
-// {
-// free(array[j]);
-// }
-// free(array);
-// return (NULL);
-// }
-// }
-
 len = 0;
 for (i = 0; i < ac ; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
 array[len++] = av[i][j];
 
-array[len++]= '\n';
+array[len++] = '\n';
 }
 
 array[len++] = '\0';
