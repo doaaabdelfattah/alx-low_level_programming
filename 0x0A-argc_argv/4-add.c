150e7f20eef9e2e3f29ad1073ee77b254c4cd350
Doaa Abdelfattah <doaaabdelfattah@Doaas-MacBook-Pro.local>
2023-09-07 00:34:11 +0300

    fix error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - main function
  * @argc: count
@@ -9,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int i;
+int i, j;
 int count = 0;
 
 if (argc == 1)
@@ -17,20 +18,26 @@ printf("0\n");
 
 else
 {
-for (i = 1; i < argc ; i++)
+for (i = 1; i < argc; i++)
 {
-if (!atoi(argv[i]))
+	for (j = 0; argv[i][j]; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+}
+}
+for (i = 1; i < argc; i++)
 {
-printf("Error\n");
-return (1);
-}
-if (atoi(argv[i]) > 0)
-{
-count = count + atoi(argv[i]);
-}
-}
-printf("%d\n", count);
+	if (atoi(argv[i]) > 0)
+	{
+	count = count + atoi(argv[i]);
+	}
 }
 
+printf("%d\n", count);
 return (0);
+}
 }
