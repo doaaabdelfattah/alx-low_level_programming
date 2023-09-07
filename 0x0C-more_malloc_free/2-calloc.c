80742fe654cc46b35057c629cd57e76883931946
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-09-07 15:06:44 -0700

    “[0x0C-more_malloc_free]”

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *ptr;
 unsigned int i;
 if (nmemb == 0 || size == 0)
 	return (NULL);
-ptr = malloc(sizeof(int) * size);
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
 	return (NULL);
 for (i = 0; i < nmemb; i++)
