d25ace142ac76047c0dceeb66b4822ae91438d2f
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-09-06 17:02:39 -0700

    “[0x07-pointers_arrays_strings]”

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,12 @@ void print_diagsums(int *a, int size)
 int i;
 int d1 = 0;
 int d2 = 0;
-for (i = 0; i < size; i++)
+for (i = 0; i < size * size; i++)
 {
-{
-d1 = d1 + a[i];
+if (i % (size + 1) == 0)
+	d1 = d1 + a[i];
+if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+	d2 = d2 + a[i];
 }
-}
-printf("%i, %i", d1, d2);
+printf("%d, %d\n", d1, d2);
 }
