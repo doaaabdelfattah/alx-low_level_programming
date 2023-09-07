b0a86d889e7ef96c142059de62d06075b106b755
“doaaabdelfattah” <doaa.abdalfattah@gmail.comgit config --global user.name doaaabdelfattahgit config --global user.email doaa.abdalfattah@gmail.com>
2023-09-07 15:56:04 -0700

    “[0x0C-more_malloc_free]”

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,4 +6,20 @@
  * @max: end
  * Return: pointer
  */
+int *array_range(int min, int max)
+{
+int *ptr;
+int i, size;
+if (min > max)
+return (NULL);
+
+size = (max - min) + 1;
+ptr = malloc(size *sizeof(int));
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+	ptr[i] = min + i;
+return (ptr);
+}
 
