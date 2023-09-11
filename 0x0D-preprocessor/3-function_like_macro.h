cd357e222587e30db6beb2e0ef78410b82e6e1ef
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-11 04:17:05 -0700

    “[0x0D-preprocessor]”

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,10 @@
-#ifndef 3-function_like_macro
-#ifdef 3-function_like_macro
+#ifndef function_like_macro_h
+#define function_like_macro_h
+
+/**
+ * ABS - function like macro
+ * @x: input
+ */
 
 #define ABS(x) (x > 0 ? (x) : (-x))
 
