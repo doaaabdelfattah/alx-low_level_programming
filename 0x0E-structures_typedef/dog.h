672fdbfb350b3cb815527d849e1268856a70de76
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-11 02:45:12 -0700

    “[0x0E-structures_typedef]”

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,12 @@ struct dog
 	char *owner;
 };
 
-/**
- * struct dog - Short description
- * @name: dog name
- * @age: Second member
- * @owner: Third member
- */
-typedef struct dog dog_t;
+typedef struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
