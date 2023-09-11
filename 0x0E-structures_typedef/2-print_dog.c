7c3e709ce0b0f5d8be74894410b038f539a232f7
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-11 02:13:19 -0700

    “[0x0E-structures_typedef]”

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - print function
+* @d: pinter to struct
+*/
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if ( d->name == NULL)
+printf("(nil)\n");
+else
+printf("Name: %s\n",d->name);
+
+if (d->age == 0)
+printf("(nil)\n");
+else
+printf("Age: %f\n",d->age);
+
+if (d->owner == NULL)
+printf("(nil)\n");
+else
+printf("Owner: %s\n",d->owner);
+}
+}
