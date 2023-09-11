18d7843d3ab910b20833bc97caad07de56e0df1f
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-11 03:01:27 -0700

    “[0x0E-structures_typedef]”

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,16 +13,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *ptr;
 dog_t my;
 ptr = &my;
-if(name != NULL && owner != NULL && age > 0)
+if(name == NULL || owner == NULL || age < 0)
 {
+	return (NULL);
+}
 strcpy(ptr->name, name);
 strcpy(ptr->owner, owner);
 ptr->age = age;
 return (ptr);
-}
-else
-return (NULL);
-
-
 
 }
