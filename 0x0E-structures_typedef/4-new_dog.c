fddca98d0ff9af853f179ff2d507e2c7af4f2235
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-11 02:57:54 -0700

    “[0x0E-structures_typedef]”

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* new_dog - create new dog
+* @name: name
+* @age: age
+* @owner: owner
+* Return: pinter to struct
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *ptr;
+dog_t my;
+ptr = &my;
+if(name != NULL && owner != NULL && age > 0)
+{
+strcpy(ptr->name, name);
+strcpy(ptr->owner, owner);
+ptr->age = age;
+return (ptr);
+}
+else
+return (NULL);
+
+
+
+}
