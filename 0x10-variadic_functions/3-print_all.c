06dd5aa47c3b6080facd0fdcf63a111cc1d87472
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-15 16:26:11 -0700

    “[0x10-variadic_functions]”

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,34 +2,48 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_all - print anything
- * @format: format
- */
+* print_all - print anything
+* @format: format
+*/
 void print_all(const char * const format, ...)
 {
-	int i;
-	va_list input;
-	va_start(input, format);
-	i = 0;
-	while (format[i] != '\0')
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(input, int));
-				break;
-                        case 's':
-                                printf("%s", va_arg(input, char *));
-                                break;
-                        case 'i':
-                                printf("%d", va_arg(input, int));
-                                break;
-                        case 'f':
-                                printf("%f", va_arg(input, double));
-                                break; 
-		}
-		i++;
-	}
-	printf("\n");
+int i, flag;
+char *str;
+va_list input;
+va_start(input, format);
+i = 0;
+while (format[i] != '\0')
+{
+flag = 0;
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(input, int));
+flag = 1;
+break;
+case 's':
+str = va_arg(input, char*);
+if (str == NULL)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+flag = 1;
+break;
+case 'i':
+printf("%d", va_arg(input, int));
+flag = 1;
+break;
+case 'f':
+printf("%f", va_arg(input, double));
+flag = 1;
+break; 
+}
+if (flag == 1)
+printf(", ");
+i++;
+}
+printf("\n");
 }
 
