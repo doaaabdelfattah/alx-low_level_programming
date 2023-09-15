2baa9c2da7b9bcf7d114f492f50e9fec1e294407
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-15 16:37:19 -0700

    “[0x10-variadic_functions]”

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,9 +12,12 @@ char *str;
 va_list input;
 va_start(input, format);
 i = 0;
-while (format[i] != '\0')
+while (format[i] != '\0') 
 {
-flag = 0;
+if (flag == 1 && (format[i] == 'c'||
+        format[i] == 's' ||format[i] == 'i' || format[i] == 'f')) 
+printf(", ");
+
 switch (format[i])
 {
 case 'c':
@@ -40,8 +43,6 @@ printf("%f", va_arg(input, double));
 flag = 1;
 break; 
 }
-if (flag == 1)
-printf(", ");
 i++;
 }
 printf("\n");
