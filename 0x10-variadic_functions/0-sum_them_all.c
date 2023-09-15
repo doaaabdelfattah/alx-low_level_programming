eedcd0fa0f067533fa6fbeddfabdf1e71a052000
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-15 02:47:12 -0700

    “[0x10-variadic_functions]”

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,22 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * sum_them_all - sumfunction
- * @n: number of int
- * Return: sum
- */
+* sum_them_all - sumfunction
+* @n: number of int
+* Return: sum
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
 int sum;
 va_list sumptr;
 if (n == 0)
-        return (0);
-va_start (sumptr, n);
+return (0);
+va_start(sumptr, n);
 i = 0;
 sum = 0;
-for(i = 0; i < n; i++)
-sum+= va_arg(sumptr, int);
+for (i = 0; i < n; i++)
+sum += va_arg(sumptr, int);
 return (sum);
 }
 
