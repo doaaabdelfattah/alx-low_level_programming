0473d15c60be41ba81b093dd4a9b0632c43592f9
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-22 15:08:53 -0700

    “[0x12-singly_linked_lists]”

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* print_list - print all element
+* @h: head pointer
+* Return: number of element
+*/
+
+size_t print_list(const list_t *h)
+{
+int count;
+count = 0;
+while (h != NULL)
+{
+count++;
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+
+h = h->next;
+}
+return (count);
+}
