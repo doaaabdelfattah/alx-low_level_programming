d028d006bb23ce49adafb15f4851e0417abcc74b
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-22 16:11:18 -0700

    “[0x12-singly_linked_lists]”

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,11 +24,16 @@ tmp->str = new_string;
 tmp->next = NULL;
 tmp->len = strlen(str);
 
+if (*head == NULL)
+	*head = tmp;
+else
+{
 ptr = *head;
 while (ptr->next != NULL)
 {
 ptr = ptr->next;
 }
 ptr->next = tmp;
+}
 return (tmp);
 }
