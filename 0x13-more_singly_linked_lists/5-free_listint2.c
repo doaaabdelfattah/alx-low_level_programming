48867c7344e2d6c9e8becc6b9d2f03b0e90ba190
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-26 04:33:38 -0700

    “[0x13-more_singly_linked_lists]”

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,10 +6,13 @@
 void free_listint2(listint_t **head)
 {
 listint_t *tmp;
+if (*head == NULL)
+	return
 while (*head != NULL)
 {
 tmp = *head;
 *head = (*head)->next;
 free(tmp);
 }
+head = NULL;
 }
