3a61028381bf278d85762335bb8f9a412f1301aa
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-26 03:56:15 -0700

    “[0x13-more_singly_linked_lists]”

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,12 +11,7 @@ listint_t *new_node;
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
-/*if (*head == NULL)
-{
-*head = new_node;
-return (new_node);
-}
-*/
+
 new_node->n = n;
 new_node->next = *head;
 *head = new_node;
