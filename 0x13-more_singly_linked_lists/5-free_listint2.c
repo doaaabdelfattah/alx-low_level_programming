f316a3d7bdb0d1164bc1c30aa569f3eeff7bf7ab
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-26 04:31:38 -0700

    “[0x13-more_singly_linked_lists]”

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: head of the list
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+if (*head == NULL)
+	return;
+while (*head != NULL)
+{
+tmp = *head;
+*head = (*head)->next;
+free(tmp);
+}
+}
