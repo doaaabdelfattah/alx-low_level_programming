694146a7b8dd7a0de0b4ae7cf6ff175ef097e7c2
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-26 05:37:50 -0700

    “[0x13-more_singly_linked_lists]”

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert mode at index
+ * @head: pointer to head pointer
+ * @idx: index
+ * @n: data
+ * Return: address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *new_node, *tmp, *prev;
+unsigned int i;
+if (*head == NULL)
+	return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+	return (NULL);
+
+new_node->n = n;
+
+tmp = prev = *head;
+for (i = 0; i < idx && tmp != NULL; i++)
+{
+	prev = tmp;
+	tmp = tmp->next;
+}
+if (i < idx || tmp == NULL)
+	return (NULL);
+prev->next = new_node;
+new_node->next = tmp->next;
+return (new_node);
+}
