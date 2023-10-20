25502486e33b0f2619ecd94e558cbd52a8dac9df
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-10-20 20:05:34 +0000

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*flip_bits - num of bits
+*@n: first num
+*@m: second num
+*Return: number of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int count;
+unsigned long int result;
+result = n ^ m;
+count = 0;
+
+while (result > 0)
+{
+if ((result & 1) > 0)
+{
+count++;
+result = result >> 1;
+}
+}
+return (count);
+}
