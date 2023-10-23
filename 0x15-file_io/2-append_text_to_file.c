563245f10c0ddc566b0f84b28341bb89fd553014
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-10-23 08:40:45 +0000

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,8 +21,13 @@ o_value = open(filename, O_WRONLY | O_APPEND);
 
 if (text_content == NULL)
 {
+if (o_value > 0)
+{
 close(o_value);
 return (1);
+}
+if (o_value == -1)
+return (-1);
 }
 /* READ THE FILE */
 w_value = write(o_value, text_content, len);
