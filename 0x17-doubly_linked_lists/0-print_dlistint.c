afa42d3d6781f93cf44b9dfa3aa5d1351f9eadae
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-09 19:49:10 +0000

    “upload”

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that prints all the elements of list.
+ * @h: pointer to the head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *ptr;
+size_t count;
+count = 0;
+ptr = h;
+if (h == NULL)
+{
+return (0);
+}
+while (ptr != NULL)
+{
+printf("%d\n", ptr->n);
+count++;
+ptr = ptr->next;
+}
+return (count);
+}
