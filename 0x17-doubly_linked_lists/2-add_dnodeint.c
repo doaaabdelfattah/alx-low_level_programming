581348b954864bf41531a7fa970650436106d6f8
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-09 19:58:03 +0000

    “upload”

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node at the beginning
+ * @head: pointer to the head
+ * @n: data
+ * Return: address of the new element or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+/* Create new node */
+new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+/* Connect both sides */
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
