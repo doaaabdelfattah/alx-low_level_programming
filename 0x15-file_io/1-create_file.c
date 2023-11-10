2b7142aedf36fe6e9944887ff2be2a9bdd0846b1
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 21:04:43 +0000

    “upload”

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _strlen - function that returns the length of a string
-* @s : string
-*
-* Return: lenght of string
-*/
+ * _strlen - function that returns the length of a string
+ * @s : string
+ *
+ * Return: lenght of string
+ */
 
 int _strlen(char *s)
 {
@@ -18,7 +18,7 @@ return (i);
 }
 
 /**
-*create_file - create file
+ *create_file - create file
 *@filename: file name
 *@text_content: letters
 *Return: number of letters printed if Success
@@ -30,11 +30,10 @@ int o_value, w_value, len;
 
 if (filename == NULL)
 return (-1);
-
 /* Ctreate empty file */
 if (text_content == NULL)
 {
-o_value = open(filename, O_CREAT, 0600);
+o_value = open(filename, O_CREAT | O_WRONLY, 0644);
 if (o_value == -1)
 return (-1);
 close(o_value);
@@ -47,14 +46,17 @@ continue;
 /* with file permission read and write */
 o_value = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 /* Return File discrptor */
-
+if (o_value == -1)
+return (-1);
 /* READ THE FILE */
 w_value = write(o_value, text_content, len);
 /* Return Number of bytes read on success*/
 
-if (o_value == -1 || w_value == -1)
+if (w_value == -1)
+{
+close(o_value);
 return (-1);
-
+}
 close(o_value); /* Close open file*/
 return (1);
 }
