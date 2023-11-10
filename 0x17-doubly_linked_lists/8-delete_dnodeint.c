c6f8e716d26acbc9f6becd10a95b63b67529a429
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 19:06:26 +0000

    “upload”

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,41 +7,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *ptr;
-	unsigned int i;
-	/* Check if list empty */
-	if (*head == NULL)
-		return (-1);
+dlistint_t *ptr;
+unsigned int i;
+/* Check if list empty */
+if (*head == NULL)
+return (-1);
 
-	if (index == 0)
-	{
-		ptr = *head;
-		*head = (*head)->next;
-		if (*head != NULL)
-		{
-			(*head)->prev = NULL;
-		}
-        free(ptr);
-	}
-	else
-	{
-		ptr = *head;
-		/* Iterate through nodes */
-		for (i = 0; i < index; i++)
-		{
-			/* Indicate failure for an index out of bounds */
-			if (ptr == NULL)
-				return (-1);
-			ptr = ptr->next;
-		}
-		/* Link nodes */
-		if (ptr->next != NULL)
-			ptr->prev->next = ptr->next;
-		if (ptr->prev != NULL)
-			ptr->next->prev = ptr->prev;
-		else
-			*head = ptr->next;
-        free(ptr);
-	}
-	return (1);
+if (index == 0)
+{
+ptr = *head;
+*head = (*head)->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+
+free(ptr);
+}
+else
+{
+ptr = *head;
+/* Iterate through nodes */
+for (i = 0; i < index; i++)
+{
+/* Indicate failure for an index out of bounds */
+if (ptr == NULL)
+return (-1);
+ptr = ptr->next;
+}
+if (ptr == NULL)
+return (-1);
+/* Link nodes */
+/* Check if this node is the first node */
+if (ptr->prev != NULL)
+ptr->prev->next = ptr->next;
+else
+*head = ptr->next;
+if (ptr->next != NULL)
+ptr->next->prev = ptr->prev;
+free(ptr);
+}
+return (1);
 }
