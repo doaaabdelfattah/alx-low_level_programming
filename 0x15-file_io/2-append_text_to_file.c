9654f2c73f31e8004431ad21999a540ca5f5e7fb
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-10 20:59:40 +0000

    “upload”

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*append_text_to_file - create file
+ *append_text_to_file - create file
 *@filename: file name
 *@text_content: letters
 *Return: 1 if success
@@ -12,6 +12,8 @@ int o_value, w_value, len;
 
 if (filename == NULL)
 return (-1);
+if (text_content == NULL)
+return (1);
 
 for (len = 0; text_content[len] != '\0'; len++)
 continue;
@@ -19,23 +21,18 @@ continue;
 /* with file permission read and write */
 o_value = open(filename, O_WRONLY | O_APPEND);
 
-if (text_content == NULL)
-{
-if (o_value > 0)
-{
-close(o_value);
-return (1);
-}
 if (o_value == -1)
 return (-1);
-}
+
 /* READ THE FILE */
 w_value = write(o_value, text_content, len);
 /* Return Number of bytes read on success*/
 
-if (o_value == -1 || w_value == -1)
+if (w_value == -1)
+{
+close(o_value);
 return (-1);
-
+}
 close(o_value); /* Close open file*/
 return (1);
 }
