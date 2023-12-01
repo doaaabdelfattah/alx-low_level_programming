3e7aa88118aa31b8739691d35ebac76d390c9277
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-01 20:35:06 +0000

    “upload”

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 int index;
 hash_node_t *node;
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
 index = key_index((unsigned char *)key, ht->size);
 node = ht->array[index];
 
