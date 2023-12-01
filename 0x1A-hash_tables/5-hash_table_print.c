f6c4921898e638fcd7dacd63ae16db573cc52512
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-01 21:00:21 +0000

    “upload”

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print hash table
+ * @ht: pointer to hash table.
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned int i;
+hash_node_t *node;
+int flag;
+flag = 0;
+
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i])
+{
+if (flag)
+printf(", ");
+node = ht->array[i];
+
+while (node)
+{
+printf("'%s': '%s'", node->key, node->value);
+node = node->next;
+if (node != NULL)
+printf(", ");
+}
+flag = 1;
+}
+}
+printf("}\n");
+}
